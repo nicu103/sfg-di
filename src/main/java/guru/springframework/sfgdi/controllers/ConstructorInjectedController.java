16b60ff963f4c311fbcbadff347ae98dee716313
nicu <no@mail.com>
2020-07-22 00:03:48 +0300

    Allow spring to manage the SetterInjectedController and the ConstructorInjectedController

--- src/main/java/guru/springframework/sfgdi/controllers/ConstructorInjectedController.java
+++ src/main/java/guru/springframework/sfgdi/controllers/ConstructorInjectedController.java
@@ -1,9 +1,12 @@
 package guru.springframework.sfgdi.controllers;
 
 import guru.springframework.sfgdi.services.GreetingService;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class ConstructorInjectedController {
     private final GreetingService greetingService;
+    // The @Autowired annotation is optional for Constructor dependency injection
     public ConstructorInjectedController(GreetingService greetingService) {
         this.greetingService = greetingService;
     }
